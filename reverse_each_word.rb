f543af763e3b3cb7a0db083871635e31f2fc084c
Cameron Bisca <cjbisca@gmail.com>
2020-11-09 22:54:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,11 @@ def reverse_each_word(sentence)
   new_array.join(" ")
 end 
 
-def reverse_each_word(sentence) 
-  sentence = sentence.split 
+# def reverse_each_word(sentence) 
+#   sentence = sentence.split 
   
-  sentence.map do |word|
-    word.reverse 
-  end 
-  sentence 
-end 
+#   sentence.map do |word|
+#     word.reverse 
+#   end 
+#   sentence 
+# end 
